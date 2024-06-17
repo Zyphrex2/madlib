ef8009ba34896b1c173dadc2339432a22816de94
Zyphrex2 <anthonycompean749@gmail.com>
2024-06-16 22:25:06 -0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,16 @@
+
+celebrity = input("enter a celebrity ")
+person = input("enter a person ")
+noun1 = input("enter a noun ")
+noun2 = input("enter ANOTHER noun ")
+noun3 = input("enter another noun i dont use adjectives like that ")
+
+
+
+
+
+
+print("Once upon a time there was a person that went by the name " + celebrity + ".")
+print("This person hated " + noun1 + " more than anything, in fact they would get rid of it if they could")
+print("Their enemy " + person + " loves " + noun1 + ", and they hate " + noun2 + ".")
+print("The only thing they have in common is their love for " + noun3 + ".")
